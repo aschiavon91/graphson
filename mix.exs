19954946f09b13cbff3a497ca1babab0bd240d96
Antonio <aschiavon91@gmail.com>
2022-12-13 20:24:37 -0300

    feat: add ex_doc

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Graphson.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.0"}
+      {:jason, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
